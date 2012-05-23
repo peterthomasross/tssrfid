ed894bc1a2e48e2f62414187a410f047f32c52f3
Pete Ross <peter.ross@live.com>
2012-05-23 13:36:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tssrfid::Application.config.secret_token = 'b182a404aac9ce012ae43a9e6b69a8d501c5bdedd0fa314eee16c6413c282dcc56bc26c1cad87bf9c112d36816a2d15500a5892470bba2712a25cf3fa275a9b9'
